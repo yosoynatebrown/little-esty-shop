7bc82184d26202b2412d4ebd89bac8d629838119
Nathan Brown <nathan.brown263@gmail.com>
2021-11-13 16:47:40 -0700

    feat: Add validations for bulk discounts

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe BulkDiscount, type: :model do
    describe 'relationships' do
     it {should belong_to :merchant}
   end
+    describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:percent_discount) }
+    it { should validate_presence_of(:quantity_threshold) }
+  end
 end
