44a287588f1e16aecf09c1e1d2b086329ef665c5
Nathan Brown <nathan.brown263@gmail.com>
2021-11-14 10:47:28 -0700

    Add seeds file to generate bulk discounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  discount = BulkDiscount.create!(
+    name: "#{Faker::Company.bs.titleize} Discount",
+    percent_discount: Faker::Number.decimal(l_digits: 0, r_digits: 2),
+    quantity_threshold: Faker::Number.between(from: 1, to: 10),
+    merchant: Merchant.order('RANDOM()').first)
+end
