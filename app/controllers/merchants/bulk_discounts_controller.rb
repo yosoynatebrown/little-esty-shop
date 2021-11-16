a860f28e2d1e55ef31c5ba6b0f41b591daf1d720
Nathan Brown <nathan.brown263@gmail.com>
2021-11-15 18:04:25 -0700

    feat: Add holiday discount view link extension

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -5,27 +5,27 @@ class Merchants::BulkDiscountsController < ApplicationController
     @holidays = HolidayService.next_3_holidays
   end
 
-  # GET /bulk_discounts/1 or /bulk_discounts/1.json
   def show
     set_bulk_discount
   end
 
-  # GET /bulk_discounts/new
   def new
     @merchant = Merchant.find(params[:id])
     if params[:holiday_name]
-      @bulk_discount = @merchant.bulk_discounts.new(name: params[:holiday_name], percent_discount: params[:percent_discount], quantity_threshold: params[:quantity_threshold])
+      @bulk_discount = @merchant.bulk_discounts.new(
+        name: params[:holiday_name],
+        percent_discount: params[:percent_discount],
+        quantity_threshold: params[:quantity_threshold]
+                                                   )
     else
       @bulk_discount = @merchant.bulk_discounts.new
     end
   end
 
-  # GET /bulk_discounts/1/edit
   def edit
     set_bulk_discount
   end
 
-  # POST /bulk_discounts or /bulk_discounts.json
   def create
     @bulk_discount = BulkDiscount.new(bulk_discount_params)
     @merchant = Merchant.find(@bulk_discount.merchant.id)
@@ -40,7 +40,6 @@ class Merchants::BulkDiscountsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /bulk_discounts/1 or /bulk_discounts/1.json
   def update
     set_bulk_discount
 
@@ -54,7 +53,6 @@ class Merchants::BulkDiscountsController < ApplicationController
     end
   end
 
-  # DELETE /bulk_discounts/1 or /bulk_discounts/1.json
   def destroy
     set_bulk_discount
     @bulk_discount.destroy
@@ -65,12 +63,10 @@ class Merchants::BulkDiscountsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_bulk_discount
       @bulk_discount = BulkDiscount.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def bulk_discount_params
       params.require(:bulk_discount).permit(:percent_discount, :quantity_threshold, :merchant_id, :name)
     end
